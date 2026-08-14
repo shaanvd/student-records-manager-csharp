a4b88e3a8fbd4c7f958b76987d1e17073bcd06b5
shaanvd <shaanv101@gmail.com>
2026-08-14 14:55:30 +0100

    fixed syntax error

--- StudentRecords/Program.cs
+++ StudentRecords/Program.cs
@@ -43,7 +43,7 @@ while (true)
     }
     catch (ArgumentException ex)
     {
-        Console.WriteLine($"Validation error: {ex.Message}")
+        Console.WriteLine($"Validation error: {ex.Message}");
     }
     catch (InvalidOperationException ex)
     {
